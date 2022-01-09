77fe19d1569766b96428ac3b3b5b202d06bea8b1
Trương Thu Thảo <62806971+truongthuthao651@users.noreply.github.com>
2022-01-09 10:02:01 +0700

    Add files via upload

---
+++ Slides/Phím tắt.sql
@@ -0,0 +1,17 @@
+Các phím tắt hay sử dụng trong sql
+
+1. Ctrl + N: mở cửa sổ query mới.
+2. Ctrl + Tab: hiển thị các cửa sổ đang làm việc.
+3. Ctrl + R: ẩn hiện kết quả query
+4. Ctrl + E/ F5: run câu lệnh sql được chỉ định.
+5. Alt + Break or Alt + Scroll Lock: Hủy bỏ câu lệnh đang chạy.
+6. Ctrl + Shift + U, Ctrl + Shift + L: chuyển câu lệnh chọn thành chữ hoa/chữ thường.
+7. Ctrl + L: hiển thị quá trình chạy câu lệnh.
+8. Ctrl + M: hiển thị kết quả và quá trình chạy câu lệnh.
+9. Ctrl + Space, Tab: hiển thị hint
+9. Ctrl + Space, Tab: hiển thị hint
+10. Ctrl + G: goto đến dòng chỉ định
+11. Ctrl + F: Tìm kiếm 
+12. Ctrl + K & Ctrl + C , Ctrl + K & Ctrl + U: comment và bỏ comment dòng lệnh
+13. Ctrl + C: copy kết quả
+14. Ctrl + Shift+ C:copy cả tiêu tên cột của bảng.
